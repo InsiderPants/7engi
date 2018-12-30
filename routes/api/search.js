02ccb468567547eff702eac3a3a5ae45648de46d
Ajay Dabas <se.mailforwork@gmail.com>
2018-12-30 15:28:01 +0530

    minor fixes

--- routes/api/search.js
+++ routes/api/search.js
@@ -7,16 +7,16 @@ const express = require("express"),
 // @access : public route
 router.post("/getResults",(req,res)=>{
 	// take data from request
-	const gradeMA102 = req.body.gradeMA102,
-		  gradeAP102 = req.body.gradeAP102,
-		  gradeEE102 = req.body.gradeEE102,
-		  gradeCO102 = req.body.gradeCO102,
-		  gradeME102 = req.body.gradeME102,
-		  gradeEN102 = req.body.gradeEN102,
-		  cgpa = req.body.cgpa;
+	const gradeMA102 = Number(req.body.ma102),
+		  gradeAP102 = Number(req.body.ap102),
+		  gradeEE102 = Number(req.body.ee102),
+		  gradeCO102 = Number(req.body.co102),
+		  gradeME102 = Number(req.body.me102),
+		  gradeEN102 = Number(req.body.en102),
+		  cgpa = (gradeMA102+gradeAP102+gradeEE102+gradeCO102+gradeME102+gradeEN102)/6;
 	// concatenate the features
 	features = [gradeMA102,gradeAP102,gradeEE102,gradeCO102,gradeME102,gradeEN102,cgpa];
-	
+	console.log(features);
 	// do similarity search
 	results = knnSearch(features);
 
