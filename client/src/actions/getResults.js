7839db602353de2df15597d82af02a98e76a0795
Manish Pant <30862627+InsiderPants@users.noreply.github.com>
2019-01-02 20:28:59 +0530

    created results page

--- client/src/actions/getResults.js
+++ client/src/actions/getResults.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
-import {GET_ERRORS, GET_RESULTS, LOADING_RESULTS} from './types';
+import { GET_ERRORS, GET_RESULTS, LOADING_RESULTS } from './types';
 
-export const getResults = (data)=>(dispatch)=>{
+export const getResults = (data, history)=>(dispatch)=>{
 	dispatch({type:LOADING_RESULTS});
 	axios.post('/api/search/getResults', data)
-		.then(res => 
+		.then(res => {
 			dispatch({
 				type:GET_RESULTS,
 				payload:res.data})
-		)
+			history.push('/results');
+		})
 		.catch(err => 
 			dispatch({
 				type:GET_ERRORS,
--- client/src/components/result/result.js
+++ client/src/components/result/result.js
@@ -1,19 +1,60 @@
 import React from 'react';
-import Navbar from '../navbar/Navbar';
-import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import {compose} from 'redux';
+import { withStyles } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import ResultsCard from '../../templates/ResultsCard'; 
+
+
+
+const style = theme => ({
+    root: {
+        flexGrow: 1,
+        margin: '2%',
+    },
+})
 
 class Result extends React.Component {
+    
+    showResults() {
+        if(this.props.results.length >0)
+        {
+            const resultArray = this.props.results;
+            return resultArray.map((item,i) => {
+                return (
+                    <Grid item xs={12} sm={3}>
+                        <ResultsCard 
+                            key={i}
+                            data={item}
+                        />
+                    </Grid>
+                    )
+            });
+        }
+        else {
+            
+        }
+    }
+    
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <Navbar/>
-                result
-                <Link to="/">
-                    <button>home</button>
-                </Link>
+            <div className={classes.root}>
+                <Grid container justify = "center" alignItems = "center" spacing={32}>
+                    {this.showResults()}
+                </Grid> 
             </div>
         );
     }
 }
 
-export default Result;
+const mapStateToProps = (state) => ({
+    results: state.results.resultData
+});
+
+export default compose(
+    connect(mapStateToProps),
+    withRouter,
+    withStyles(style)
+)(Result);
